1f63e8f3066934d5d295da7b56e0bb31387a7c6a
Alan Russell <ajrussellaudio@gmail.com>
2017-09-09 18:53:05 +0100

    add remove first method

--- bus_stop.py
+++ bus_stop.py
@@ -2,3 +2,12 @@ class BusStop():
     def __init__(self, location):
         self.location = location
         self.queue = []
+
+    def queue_length(self):
+        return len(self.queue)
+
+    def add(self, person):
+        self.queue.append(person)
+
+    def remove(self):
+        return self.queue.pop(0)
--- bus_stop_test.py
+++ bus_stop_test.py
@@ -10,5 +10,23 @@ class BusStopTest(unittest.TestCase):
     def test_location(self):
         self.assertEqual(self.bus_stop.location, "Partick Interchange")
 
+    def test_queue_length(self):
+        self.assertEqual(self.bus_stop.queue_length(), 0)
+
+    def test_add(self):
+        person = Person("Alan", 35)
+        self.bus_stop.add(person)
+        self.assertEqual(self.bus_stop.queue_length(), 1)
+
+    def test_remove(self):
+        person1 = Person("Alan", 35)
+        person2 = Person("Marj", 31)
+        self.bus_stop.add(person1)
+        self.bus_stop.add(person2)
+        removed = self.bus_stop.remove()
+        self.assertEqual(removed, person1)
+        self.assertEqual(self.bus_stop.queue_length(), 1)
+
+
 if __name__ == '__main__':
     unittest.main()
